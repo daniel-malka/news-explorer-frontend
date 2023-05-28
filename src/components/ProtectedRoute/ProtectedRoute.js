76bba9a3ca3d09dbee6ecf3081d5ed6a5c74b463
Daniel <daniel847@tutanota.com>
2023-05-28 12:54:01 +0300

    delete src

--- src/components/ProtectedRoute/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../../contexts/AuthContext';
-
-const ProtectedRoute = ({ children }) => {
-  const { isLoggedIn } = useAuth();
-  return isLoggedIn ? children : <Navigate to="/" replace />;
-};
-
-export default ProtectedRoute;
