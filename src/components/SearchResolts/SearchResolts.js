019e966a9ccd7a8f2569b7c8c8d5c63d7a3b9554
Daniel <daniel847@tutanota.com>
2023-05-31 18:00:26 +0300

    fix: cards logic due screen size; cards minmax response

--- src/components/SearchResolts/SearchResolts.js
+++ src/components/SearchResolts/SearchResolts.js
@@ -1,48 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NewsCard from '../NewsCard/NewsCard';
 import NotFound from '../NotFound/NotFound';
 
 const SearchResolts = (props) => {
-  const [cardsToShow, setCardsToShow] = useState(3);
-
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [cardsToShow, setCardsToShow] = useState(0);
   const visibleCards = props.searchResults.slice(0, cardsToShow);
 
   const handleShowMore = () => {
-    setCardsToShow(cardsToShow + 3);
+    if (screenWidth > 920) {
+      setCardsToShow(cardsToShow + 3);
+    } else {
+      setCardsToShow(cardsToShow + 2);
+    }
   };
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', setScreenWidth);
+    handleShowMore();
+    return () => {
+      window.removeEventListener('resize', setScreenWidth);
+    };
+  }, [screenWidth]);
 
   return (
-    <section className="searchResults">
+    <>
       {props.searchTerm.length === 0 ? (
-        <div className="searchResults__div" style={{ display: 'none' }}></div>
+        ''
       ) : (
-        <div className="searchResults__div">
-          <h3 className="searchResult-title">Search results</h3>
-          <div className="searchResult__container">
-            {visibleCards.length !== 0
-              ? visibleCards.map((card) => (
-                  <div key={card._id} className="searchResult__cards-listItem">
-                    <NewsCard card={card} />
-                  </div>
-                ))
-              : props.handleSearchClicked && (
-                  <div className="searchResult__cards-listItem">
-                    <NotFound />
-                  </div>
-                )}
-            {!props.showMore &&
-              visibleCards.length < props.searchResults.length && (
+        <section className="searchResults">
+          <div className="searchResults__div">
+            <h3 className="searchResult-title">Search results</h3>
+            <div className="searchResult__container">
+              {visibleCards.length !== 0
+                ? visibleCards.map((card) => (
+                    <div
+                      key={card._id}
+                      className="searchResult__cards-listItem"
+                    >
+                      <NewsCard card={card} />
+                    </div>
+                  ))
+                : props.handleSearchClicked && (
+                    <div className="searchResult__cards-listItem">
+                      <NotFound />
+                    </div>
+                  )}
+              {!props.showMore &&
+              visibleCards.length < props.searchResults.length &&
+              screenWidth <= `920` ? (
                 <button
                   onClick={handleShowMore}
                   className="newsCardList__button"
                 >
                   Show more
                 </button>
+              ) : (
+                ''
               )}
+            </div>
           </div>
-        </div>
+          {screenWidth > `920` ? (
+            <button onClick={handleShowMore} className="newsCardList__button">
+              Show more
+            </button>
+          ) : (
+            ''
+          )}
+        </section>
       )}
-    </section>
+    </>
   );
 };
 
